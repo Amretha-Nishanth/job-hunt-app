f92f1a92c45033a34e67c50049cd807e5cbe0dff
Nishanth Natarajan <nishanthnatarajan@Nishanths-MacBook-Pro.local>
2026-02-24 00:30:04 +0800

    Add: /ping endpoint for keep-alive

--- web_main.py
+++ web_main.py
@@ -680,6 +680,11 @@ def bookmarklet_bulk():
         resp.headers["Access-Control-Allow-Origin"] = "*"
         return resp, 500
 
+
+@app.route("/ping")
+def ping():
+    return "ok", 200
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
